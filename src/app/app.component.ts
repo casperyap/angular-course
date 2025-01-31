9afcbfe0b46e2e6977c093e85c9ea0469bd3f249
Casper Yap <casyap@gmail.com>
2025-01-31 19:26:51 +0800

    Re-implement using aysnc pipe.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,9 +14,8 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 })
 export class AppComponent implements OnInit {
 
-
-  courses;
-
+  courses$ : Observable<Course[]>;
+  
   constructor(private http: HttpClient) {
 
   }
@@ -26,10 +25,12 @@ export class AppComponent implements OnInit {
       .set("page", "1")
       .set("pageSize", "10");
 
-    this.http.get('/api/courses', {params})
-    .subscribe(
-      courses => this.courses = courses
-    );
+    this.courses$ = this.http.get<Course[]>('/api/courses', {params});
+
+    // this.http.get('/api/courses', {params})
+    // .subscribe(
+    //   courses => this.courses = courses
+    // );
   }
 
 
